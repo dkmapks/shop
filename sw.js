e1a1e3916a92bfd84a4460e3db82ede246fa1c21
dkmapks <dkmap039@gmail.com>
2025-06-12 09:15:27 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,15 @@
-const CACHE_NAME = 'fizyka-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  // Ikony jeśli będą
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open("branzo-v1").then(function (cache) {
+      return cache.addAll(["./index.html", "./manifest.json", "./icon.png"]);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
     })
   );
 });
